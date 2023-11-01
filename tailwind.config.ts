d847babac74ad964f732a1275630b2361f38ec73
Jason Gong <jasongong83@gmail.com>
2023-10-31 22:27:39 -0400

    add radix/tailwind packages and plugin. test checkbox

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
+import radixPlugin from "tailwindcss-radix";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-} satisfies Config
-
+  plugins: [animatePlugin, radixPlugin],
+} satisfies Config;

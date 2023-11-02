92886009379b842d70969be22421fb1049cba361
Jason Gong <jasongong83@gmail.com>
2023-11-02 17:20:47 -0400

    add header with log. set skeleton body and footer.

---
+++ app/components/header.tsx
@@ -0,0 +1,27 @@
+export function Header() {
+  return (
+    <div className="flex h-16 text-gray-400 font-semibold">
+      <div className="self-center flex flex-row justify-between w-full lg:mx-64 xl:mx-96">
+        <div className="flex flex-row">
+          <div className="self-center">
+            <svg
+              width="26"
+              height="22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-labelledby="asana-logo-title"
+            >
+              <title id="swft-logo-title">SWFT</title>
+              <path
+                d="M18.559 11.605a5.158 5.158 0 1 0 0 10.317 5.158 5.158 0 0 0 0-10.317Zm-13.401.001a5.158 5.158 0 1 0 0 10.315 5.158 5.158 0 0 0 0-10.315Zm11.858-6.448a5.158 5.158 0 1 1-10.316 0 5.158 5.158 0 0 1 10.316 0Z"
+                fill="#0074ff"
+              ></path>
+            </svg>
+          </div>
+          <div className="ml-2 text-2xl font-semibold text-black">SWFT</div>
+        </div>
+        <div>Log In</div>
+      </div>
+    </div>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Checkbox } from "../components/checkbox";
+import { Header } from "~/components/header";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,9 +11,14 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <div className="text-red-400">Welcome to Remix</div>
-      <div className="ml-4">
-        <Checkbox />
+      <div className="flex flex-col h-screen">
+        <Header />
+        <div className="flex flex-1">
+          <div className="mx-auto self-center">Body</div>
+        </div>
+        <div className="flex justify-items-center h-16 text-red-400 border-b-gray-800 bg-black">
+          <div className="mx-auto self-center">Footer</div>
+        </div>
       </div>
     </div>
   );

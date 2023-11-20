4a4d898cb5986703bb422983a4f0944cabe90ec7
Jason Gong <jasongong83@gmail.com>
2023-11-20 12:08:29 -0500

    add faker, bcrypt, and seed users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
@@ -15,3 +17,26 @@ await prisma.user.create({
     },
   },
 });
+
+function createSeedUserData() {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+
+  return {
+    email: faker.internet.email(),
+    name: `${firstName} ${lastName}`,
+    password: {
+      create: {
+        hash: bcrypt.hashSync(faker.internet.password(), 10),
+      },
+    },
+  };
+}
+
+const numCreateSeedUsers = 3;
+
+for (let i = 0; i <= numCreateSeedUsers; i++) {
+  await prisma.user.create({
+    data: createSeedUserData(),
+  });
+}

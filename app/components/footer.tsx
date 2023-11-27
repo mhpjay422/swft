ee6b36e519b22fda71971bb413465b5a47472d2e
Jason Gong <jasongong83@gmail.com>
2023-11-27 17:12:16 -0500

    edit footer with Link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,18 +1,21 @@
+import { Link } from "@remix-run/react";
+
 export function Footer() {
   return (
     <div className="bg-white w-full sticky bottom-0 h-16">
       <div className="flex flex-row justify-between mx-32 lg:mx-64 xl:mx-96 pt-4">
         <div>© 2023 SWFT</div>
         <div className="flex mb-4">
-          <a
+          {/* NOTE: Fix src for images */}
+          <Link
+            to="https://www.linkedin.com/in/jason-gong-79772b126/"
             className="h-8 w-8 mr-20"
-            href="https://www.linkedin.com/in/jason-gong-79772b126/"
           >
-            <img className="linkedin-img" src="linked.png" alt=""></img>
-          </a>
-          <a className="h-8 w-8" href="https://github.com/mhpjay422">
-            <img className="github-img" src="github2.png" alt=""></img>
-          </a>
+            <img className="h-8 w-8" src="linked.png" alt=""></img>
+          </Link>
+          <Link to="https://github.com/mhpjay422" className="h-8 w-8 mr-20">
+            <img className="h-8 w-8" src="github2.png" alt=""></img>
+          </Link>
         </div>
       </div>
     </div>

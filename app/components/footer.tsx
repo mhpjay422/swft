e6cae95666b1255e31d728980c27b67e445044db
Jason Gong <jasongong83@gmail.com>
2023-11-27 17:17:08 -0500

    edit link paths

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,15 +6,14 @@ export function Footer() {
       <div className="flex flex-row justify-between mx-32 lg:mx-64 xl:mx-96 pt-4">
         <div>© 2023 SWFT</div>
         <div className="flex mb-4">
-          {/* NOTE: Fix src for images */}
           <Link
             to="https://www.linkedin.com/in/jason-gong-79772b126/"
             className="h-8 w-8 mr-20"
           >
-            <img className="h-8 w-8" src="linked.png" alt=""></img>
+            <img className="h-8 w-8" src="/linked.png" alt=""></img>
           </Link>
           <Link to="https://github.com/mhpjay422" className="h-8 w-8 mr-20">
-            <img className="h-8 w-8" src="github2.png" alt=""></img>
+            <img className="h-8 w-8" src="/github2.png" alt=""></img>
           </Link>
         </div>
       </div>

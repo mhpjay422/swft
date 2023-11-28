b1dc2cb36b4cd83469cef1e391eab12c7d306217
Jason Gong <jasongong83@gmail.com>
2023-11-28 16:58:38 -0500

    edit seed file adding kaker tasks to admin

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,6 +6,13 @@ const prisma = new PrismaClient();
 
 await prisma.user.deleteMany();
 
+const createRandomTask = () => ({
+  title: faker.lorem.words(),
+  content: faker.lorem.paragraph(),
+  completed: faker.datatype.boolean(0.5),
+});
+const tasks = Array.from({ length: 3 }, createRandomTask);
+
 const seedData = async () => {
   await prisma.user.create({
     data: {
@@ -21,24 +28,12 @@ const seedData = async () => {
         create: {
           title: "Admin Project",
           tasks: {
-            create: [
-              {
-                title: "Task 1",
-                content: "Task 1 Content",
-                completed: false,
-                owner: {
-                  connect: { username: "admin" },
-                },
+            create: tasks.map((task) => ({
+              ...task,
+              owner: {
+                connect: { username: "admin" },
               },
-              {
-                title: "Task 2",
-                content: "Task 2 Content",
-                completed: true,
-                owner: {
-                  connect: { username: "admin" },
-                },
-              },
-            ],
+            })),
           },
         },
       },
@@ -48,14 +43,6 @@ const seedData = async () => {
   function createSeedUserData() {
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
-
-    const createRandomTask = () => ({
-      title: faker.lorem.words(),
-      content: faker.lorem.paragraph(),
-      completed: faker.datatype.boolean(0.5),
-    });
-
-    const tasks = Array.from({ length: 3 }, createRandomTask);
 
     return {
       email: faker.internet.email(),

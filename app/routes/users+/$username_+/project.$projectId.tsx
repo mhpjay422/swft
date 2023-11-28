88fee0d68cd64da372965af55693c80a934bad94
Jason Gong <jasongong83@gmail.com>
2023-11-28 16:58:57 -0500

    edit project detail page

--- app/routes/users+/$username_+/project.$projectId.tsx
+++ app/routes/users+/$username_+/project.$projectId.tsx
@@ -1,18 +1,85 @@
 import { DynamicErrorBoundary } from "#app/components/error-boundary.tsx";
+import prismaClient from "#app/utils/db.server.ts";
+import { invariantResponse } from "#app/utils/misc.tsx";
 import { type DataFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
-export async function loader({ params }: DataFunctionArgs) {
-  return json({});
+const prisma = prismaClient;
+
+export async function loader({ request, params }: DataFunctionArgs) {
+  const owner = await prisma.user.findFirst({
+    select: {
+      tasks: {
+        select: {
+          id: true,
+          title: true,
+        },
+      },
+    },
+    where: { username: params.username },
+  });
+
+  invariantResponse(owner, "Owner not found", { status: 404 });
+
+  return json({ owner });
 }
 
-export async function action({ request }: DataFunctionArgs) {
+export async function action({ request, params }: DataFunctionArgs) {
   return json({});
 }
 
 export default function UsersProjectDetailPage() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data.owner.tasks);
+
   return (
-    <div className="flex flex-col mx-96 w-auto h-96 mt-20 border border-gray-200 hover:border-gray-300 rounded-lg">
-      <div className="p-8 ">Project Page</div>
+    <div className="flex flex-grow flex-col h-full border border-gray-200 hover:border-gray-300 rounded-lg items-center">
+      <div className="flex flex-row py-6 px-5 mt-10">
+        <ul className="mr-6 w-64">
+          To do
+          {data.owner.tasks.map((task) => (
+            <li
+              key={task.id}
+              className="h-28 w-64 border border-gray-200 rounded-lg"
+            >
+              <p className="text-sm p-4 font-semibold">{task.title}</p>
+            </li>
+          ))}
+        </ul>
+        <ul className="mr-6 w-64">
+          Doing
+          {data.owner.tasks.map((task) => (
+            <li
+              key={task.id}
+              className="h-28 w-64 border border-gray-200 rounded-lg"
+            >
+              <p className="text-sm p-4 font-semibold">{task.title}</p>
+            </li>
+          ))}
+        </ul>
+        <ul className="mr-6 w-64">
+          Done
+          {data.owner.tasks.map((task) => (
+            <li
+              key={task.id}
+              className="h-28 w-64 border border-gray-200 rounded-lg"
+            >
+              <p className="text-sm p-4 font-semibold">{task.title}</p>
+            </li>
+          ))}
+        </ul>
+        <ul className="mr-6 w-64">
+          Todo
+          {data.owner.tasks.map((task) => (
+            <li
+              key={task.id}
+              className="h-28 w-64 border border-gray-200 rounded-lg"
+            >
+              <p className="text-sm p-4 font-semibold">{task.title}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- app/utils/user.ts
+++ app/utils/user.ts
@@ -1,7 +1,4 @@
 import { useRouteLoaderData } from "@remix-run/react";
-
-// Commented out the import statement
-// import { type loader as rootLoader } from "#app/root";
 import { type loader as rootLoader } from "#app/root.tsx";
 
 export function useLoggedInUser() {

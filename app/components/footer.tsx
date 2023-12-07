adc3f53e3d23d3c3bb10458de83ef0b75417a70d
Jason Gong <jasongong83@gmail.com>
2023-12-06 19:57:44 -0500

    fix footer styling

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="w-full sticky bottom-0 h-16 border-t border-gray-200 bg-gray-50">
       <div className="flex flex-row justify-between mx-32 lg:mx-64 xl:mx-96 pt-4">
-        <div>© 2023 SWFT</div>
+        <div className="whitespace-nowrap flex-shrink-0 mr-10">© 2023 SWFT</div>
         <div className="flex mb-4">
           <Link
             to="https://www.linkedin.com/in/jason-gong-79772b126/"

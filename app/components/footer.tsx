2998779c748b143ec79966648edbf6d7827ca9d4
Jason Gong <jasongong83@gmail.com>
2024-01-09 10:58:31 -0500

    add dev differentiator

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,10 @@
 export function Footer() {
   return (
-    <div className="w-full sticky bottom-0 h-16 border-t border-gray-200 bg-gray-50">
+    <div
+      className={`w-full sticky bottom-0 h-16 border-t border-gray-200 ${
+        process.env.NODE_ENV === "production" ? "bg-gray-50" : "bg-blue-200"
+      }`}
+    >
       <div className="flex flex-row justify-between mx-32 lg:mx-64 xl:mx-96 pt-4">
         <div className="whitespace-nowrap flex-shrink-0 mr-10">© 2023 SWFT</div>
         <div className="flex mb-4">
